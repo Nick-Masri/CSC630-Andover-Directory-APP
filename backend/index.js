1bccfda456eea948dc43ba39f9d100b6c1f4d85e
jeffreyshen19 <jshen@andover.edu>
2019-04-27 13:52:58 -0400

    add basic full text search

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ app.get("/people", function(req, res){
     .select("*")
     .modify(function(queryBuilder){
       //Each field is a comma-separated list of values (i.e. clusters, dorms, etc.)
-
       if("dorms" in req.query) queryBuilder.whereIn('dorm', req.query.dorms.split(","));
 
       if("clusters" in req.query) queryBuilder.whereIn('cluster', req.query.clusters.split(","));
@@ -30,12 +29,15 @@ app.get("/people", function(req, res){
       }));
 
       if("grades" in req.query) queryBuilder.whereIn('grade', req.query.grades.split(","));
+
+      if("search" in req.query) queryBuilder.whereRaw("position('" + req.query.search + "' in display_name) > 0 OR position('" + req.query.search + "' in email) > 0 OR position('" + req.query.search + "' in \"from\") > 0"); //TODO: improve on names, make lower case
     })
     .paginate(30, req.query.page ? parseInt(req.query.page) : 1)
     .then(function(results){
       res.json(results);
     })
     .catch(function(e){
+      console.log(e);
       res.status(500);
       res.send("ERROR");
     });
